8837f981580603b08f0391da3a9e506ade4e747d
DIMA_w <oddff@mail.ru>
2023-09-14 15:57:31 +0300

    Изменения 2
    - Добавил HTML разметку для блоков, добавил их на страницу
    - Добавил часть стилей для страници

--- index.js
+++ index.js
@@ -10,10 +10,15 @@ async function fetchData() {
     const feturedItemsMain = document.querySelector('.feturedItems-main');
     data.forEach(({ name, img, description, price }) => {
       const product = `
-      <div class="product">
-      </div>
+        <div class="product">
+          <img class="product__img" src="${img}" alt="Картинка">
+          <div class="product-info">
+            <h4 class="product-info__name">${name}</h4>
+            <p class="product-info__description">${description}</p>
+            <p class="product-info__price">$${price}</p>
+          </div>
+        </div>
       `;
-
       feturedItemsMain.insertAdjacentHTML('beforeend', product);
     });
   } catch (error) {
