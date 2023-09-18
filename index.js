6bdfc978e75a4a715e2a2d62197279e8d4e41fa8
DIMA_w <oddff@mail.ru>
2023-09-18 16:42:07 +0300

    Добавил стилей, работа над добавлением элементов в корзину

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+let cartArr = [];
+
+fetchData();
+
+// Функция добавления товара в корзину
+function addCart(data) {
+  const addCarts = document.querySelectorAll('.product-info__btn');
+
+  addCarts.forEach((el) => {
+    el.addEventListener('click', () => {
+      // localStorage.Lerts = JSON.stringify();
+    });
+  });
+}
+
 async function fetchData() {
   try {
     const responce = await fetch('data.json');
@@ -8,7 +23,7 @@ async function fetchData() {
     const data = await responce.json();
     console.log(data);
     const feturedItemsMain = document.querySelector('.feturedItems-main');
-    data.forEach(({ name, img, description, price }) => {
+    data.forEach(({ name, img, description, price }, index) => {
       const product = `
         <div class="product">
           <div class="product-img">
@@ -17,14 +32,18 @@ async function fetchData() {
           <div class="product-info">
             <h4 class="product-info__name">${name}</h4>
             <p class="product-info__description">${description}</p>
-            <p class="product-info__price">$${price}</p>
+            <div>
+              <p class="product-info__price">$${price}</p>
+              <button class="product-info__btn" id="img0${index}" type="button">В корзину</button>
+            </div>
           </div>
         </div>
       `;
       feturedItemsMain.insertAdjacentHTML('beforeend', product);
     });
+
+    addCart(data);
   } catch (error) {
     console.log(error);
   }
 }
-fetchData();
