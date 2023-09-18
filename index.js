ea2939314a3524be4fe17e4fd10e920be52e34e6
Lerts <oddff@mail.ru>
2023-09-18 23:49:00 +0300

    Добавление товара в корзину через массив объектов

--- index.js
+++ index.js
@@ -3,14 +3,76 @@ let cartArr = [];
 fetchData();
 
 // Функция добавления товара в корзину
-function addCart(data) {
+function addCartArr(data) {
   const addCarts = document.querySelectorAll('.product-info__btn');
 
-  addCarts.forEach((el) => {
-    el.addEventListener('click', () => {
-      // localStorage.Lerts = JSON.stringify();
+  addCarts.forEach((elBtn) => {
+    elBtn.addEventListener('click', () => {
+      changingClassOfButton(elBtn, 'Добавлено');
+      setTimeout(() => {
+        changingClassOfButton(elBtn, 'В корзину');
+      }, 1500);
+      console.log(elBtn.id);
+
+      // добавление товара в массив
+      console.log(
+        cartArr.find((elFind) => {
+          if (elFind.img === data[elBtn.id].img) {
+            console.log('рапва');
+          } else {
+            console.log('dsad');
+          }
+        })
+      );
+
+      cartArr.find((el) => {
+        if (el.img !== data[elBtn.id].img) {
+          cartArr.push(data[elBtn.id]);
+          addCart(data[elBtn.id]);
+          console.log('gjdnjh');
+          addLocalStorage();
+        }
+      });
+
+      if (cartArr.length === 0) {
+        cartArr.push(data[elBtn.id]);
+        addLocalStorage();
+        addCart(data[elBtn.id]);
+      }
     });
   });
+}
+
+// изменение класса у копки когда добавили в корзину
+function changingClassOfButton(btn, textBtn) {
+  btn.classList.toggle('activ__btn');
+  btn.textContent = textBtn;
+}
+
+// добавление товаров в корзине в localstorage
+function addLocalStorage() {
+  localStorage.Lerts = JSON.stringify(cartArr);
+}
+
+function addCart(obj) {
+  const cartItemsMain = document.querySelector('.cartItems-main');
+
+  const product = `
+    <div class="itemsInCart">
+      <i class="fa-solid fa-xmark"></i>
+      <div class="itemsInCart-img">
+        <img class="itemsInCart__img" src="${obj.img}" alt="Картинка" />
+      </div>
+      <div class="itemsInCart-info">
+        <h4 class="itemsInCart-info__name">${obj.name}</h4>
+        <p class="itemsInCart-info__price">Price: $${obj.price}</p>
+        <p class="itemsInCart-info__color">Color: ${obj.color}</p>
+        <p class="itemsInCart-info__size">Size: ${obj.size}</p>
+        <p class="itemsInCart-info__quantity">Quantity: <input type="number" max="10" /></p>
+      </div>
+    </div>
+      `;
+  cartItemsMain.insertAdjacentHTML('beforeend', product);
 }
 
 async function fetchData() {
@@ -34,7 +96,7 @@ async function fetchData() {
             <p class="product-info__description">${description}</p>
             <div>
               <p class="product-info__price">$${price}</p>
-              <button class="product-info__btn" id="img0${index}" type="button">В корзину</button>
+              <button class="product-info__btn" id="${index}" type="button">В корзину</button>
             </div>
           </div>
         </div>
@@ -42,7 +104,7 @@ async function fetchData() {
       feturedItemsMain.insertAdjacentHTML('beforeend', product);
     });
 
-    addCart(data);
+    addCartArr(data);
   } catch (error) {
     console.log(error);
   }
